a74f5e638fabe96ad360acfc19372f512387fe31
Sachin Raja <sachinraja2349@gmail.com>
2022-08-28 10:22:04 -0700

    add twoslash

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,28 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import remarkTwoslashPkg from 'remark-shiki-twoslash'
 
+/**
+ * @type {typeof remarkTwoslash}
+ */
+const shikiTwoslash = remarkTwoslashPkg.default
+
+const remarkTwoslash = [shikiTwoslash, { theme: 'github-dark' }]
 // https://astro.build/config
 export default defineConfig({
   site: 'https://s4n.land',
-  integrations: [tailwind(), mdx(), sitemap()],
+  markdown: {
+    syntaxHighlight: false,
+    remarkPlugins: [remarkTwoslash],
+  },
+  integrations: [
+    tailwind(),
+    mdx({
+      remarkPlugins: [remarkTwoslash],
+    }),
+    sitemap(),
+  ],
   vite: {
     ssr: {
       external: ['svgo'],
--- src/pages/notes/hello.md
+++ src/pages/notes/hello.md
@@ -6,3 +6,24 @@ pubDate: 'Aug 27 2022'
 ---
 
 Hello, welcome to my notes space!
+
+```ts twoslash
+interface IdLabel {
+  id: number /* some fields */
+}
+interface NameLabel {
+  name: string /* other fields */
+}
+type NameOrId<T extends number | string> = T extends number
+  ? IdLabel
+  : NameLabel
+// This comment should not be included
+
+// ---cut---
+function createLabel<T extends number | string>(idOrName: T): NameOrId<T> {
+  throw 'unimplemented'
+}
+
+let a = createLabel('typescript')
+//  ^?
+```
